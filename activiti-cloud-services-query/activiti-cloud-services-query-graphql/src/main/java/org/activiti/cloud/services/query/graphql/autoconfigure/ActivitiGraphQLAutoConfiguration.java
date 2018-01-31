53878cfd9003c0f1bea2eed53dd40a23f45ac318
Ryan Dawson <ryan.dawson@alfresco.com>
2018-01-31 09:38:32 +0000

    remove unused import

--- activiti-cloud-services-query/activiti-cloud-services-query-graphql/src/main/java/org/activiti/cloud/services/query/graphql/autoconfigure/ActivitiGraphQLAutoConfiguration.java
+++ activiti-cloud-services-query/activiti-cloud-services-query-graphql/src/main/java/org/activiti/cloud/services/query/graphql/autoconfigure/ActivitiGraphQLAutoConfiguration.java
@@ -31,7 +31,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportAware;
