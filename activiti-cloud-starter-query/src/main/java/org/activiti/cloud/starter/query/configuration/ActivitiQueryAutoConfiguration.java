f16904e56e36e7d5bc2cedefb02e96afcd2c352b
Salaboy <salaboy@gmail.com>
2018-04-04 10:17:26 +0100

    removing eureka from service layer

---
+++ activiti-cloud-starter-query/src/main/java/org/activiti/cloud/starter/query/configuration/ActivitiQueryAutoConfiguration.java
@@ -0,0 +1,10 @@
+package org.activiti.cloud.starter.query.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.activiti.cloud.services.query")
+public class ActivitiQueryAutoConfiguration {
+
+}
