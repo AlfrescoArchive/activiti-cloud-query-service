9d1681ab07b014ce34281cb56021cead1cdbb1e0
Ryan Dawson <ryan.dawson@alfresco.com>
2018-04-12 11:28:46 +0100

    componentscan needs to include identity

--- activiti-cloud-starter-query/src/main/java/org/activiti/cloud/starter/query/configuration/ActivitiQueryAutoConfiguration.java
+++ activiti-cloud-starter-query/src/main/java/org/activiti/cloud/starter/query/configuration/ActivitiQueryAutoConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan({"org.activiti.cloud.services.query",
-        "org.activiti.cloud.alfresco", "org.activiti.cloud.services.security"})
+        "org.activiti.cloud.alfresco", "org.activiti.cloud.services.security","org.activiti.cloud.services.identity"})
 public class ActivitiQueryAutoConfiguration {
 
 }
