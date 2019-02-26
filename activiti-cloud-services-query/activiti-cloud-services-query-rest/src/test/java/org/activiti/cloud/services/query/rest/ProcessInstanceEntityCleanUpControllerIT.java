1ce0cdfedd616aec2e13d704c7e6eabea151ba88
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2019-02-26 16:38:11 +0000

    Add test covering clean-up controller

---
+++ activiti-cloud-services-query/activiti-cloud-services-query-rest/src/test/java/org/activiti/cloud/services/query/rest/ProcessInstanceEntityCleanUpControllerIT.java
@@ -0,0 +1,129 @@
+package org.activiti.cloud.services.query.rest;
+
+
+import org.activiti.api.process.model.ProcessInstance;
+import org.activiti.api.runtime.conf.impl.CommonModelAutoConfiguration;
+import org.activiti.api.runtime.shared.security.SecurityManager;
+import org.activiti.cloud.conf.QueryRestAutoConfiguration;
+import org.activiti.cloud.services.query.app.repository.EntityFinder;
+import org.activiti.cloud.services.query.app.repository.ProcessDefinitionRepository;
+import org.activiti.cloud.services.query.app.repository.ProcessInstanceRepository;
+import org.activiti.cloud.services.query.model.ProcessInstanceEntity;
+import org.activiti.cloud.services.security.TaskLookupRestrictionService;
+import org.activiti.core.common.spring.security.policies.SecurityPoliciesManager;
+import org.activiti.core.common.spring.security.policies.conf.SecurityPoliciesProperties;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.UUID;
+
+import static org.activiti.alfresco.rest.docs.AlfrescoDocumentation.pageRequestParameters;
+import static org.activiti.alfresco.rest.docs.AlfrescoDocumentation.pagedResourcesResponseFields;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ProcessInstanceCleanUpController.class)
+@Import({
+        QueryRestAutoConfiguration.class,
+        CommonModelAutoConfiguration.class,
+})
+@EnableSpringDataWebSupport
+@AutoConfigureMockMvc(secure = false)
+@AutoConfigureRestDocs(outputDir = "target/snippets")
+@ComponentScan(basePackages = {"org.activiti.cloud.services.query.rest.assembler", "org.activiti.cloud.alfresco"})
+public class ProcessInstanceEntityCleanUpControllerIT {
+
+    private static final String PROCESS_INSTANCE_ALFRESCO_IDENTIFIER = "process-instance-alfresco";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProcessInstanceRepository processInstanceRepository;
+
+    @MockBean
+    private SecurityManager securityManager;
+
+    @MockBean
+    private EntityFinder entityFinder;
+
+    @MockBean
+    private SecurityPoliciesManager securityPoliciesManager;
+
+    @MockBean
+    private ProcessDefinitionRepository processDefinitionRepository;
+
+    @MockBean
+    private SecurityPoliciesProperties securityPoliciesProperties;
+
+    @MockBean
+    private TaskLookupRestrictionService taskLookupRestrictionService;
+
+    @Before
+    public void setUp() {
+        when(securityManager.getAuthenticatedUserId()).thenReturn("admin");
+        assertThat(entityFinder).isNotNull();
+        assertThat(securityPoliciesManager).isNotNull();
+        assertThat(processDefinitionRepository).isNotNull();
+        assertThat(securityPoliciesProperties).isNotNull();
+        assertThat(taskLookupRestrictionService).isNotNull();
+    }
+
+    @Test
+    public void deleteProcessInstancesShouldReturnAllProcessInstancesAndDeleteThem() throws Exception{
+
+        //given
+        given(processInstanceRepository.findAll(any(),
+                ArgumentMatchers.<Pageable>any())).willReturn(new PageImpl<>(Collections.singletonList(buildDefaultProcessInstance()),
+                PageRequest.of(1,
+                        10),
+                11));
+
+        //when
+        mockMvc.perform(delete("/admin/v1/process-instances?skipCount=10&maxItems=10")
+                .accept(MediaType.APPLICATION_JSON))
+                //then
+                .andExpect(status().isOk())
+                .andDo(document(PROCESS_INSTANCE_ALFRESCO_IDENTIFIER + "/list",
+                        pageRequestParameters(),
+                        pagedResourcesResponseFields()
+
+                ));
+
+        verify(processInstanceRepository).deleteAll();
+
+    }
+
+    private ProcessInstanceEntity buildDefaultProcessInstance() {
+        return new ProcessInstanceEntity("My-app", "My-app", "1", null, null,
+                UUID.randomUUID().toString(),
+                UUID.randomUUID().toString(),
+                ProcessInstance.ProcessInstanceStatus.RUNNING,
+                new Date());
+    }
+}
