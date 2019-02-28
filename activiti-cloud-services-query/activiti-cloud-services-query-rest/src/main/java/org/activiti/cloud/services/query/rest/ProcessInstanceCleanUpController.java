74ce86f1cb8ec1460ace7a7c8277bb7b2984dbca
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2019-02-28 14:09:56 +0000

    Refactor ProcessInstanceCleanUpController

--- activiti-cloud-services-query/activiti-cloud-services-query-rest/src/main/java/org/activiti/cloud/services/query/rest/ProcessInstanceCleanUpController.java
+++ activiti-cloud-services-query/activiti-cloud-services-query-rest/src/main/java/org/activiti/cloud/services/query/rest/ProcessInstanceCleanUpController.java
@@ -1,26 +1,27 @@
 package org.activiti.cloud.services.query.rest;
 
 import com.querydsl.core.types.Predicate;
-import org.activiti.cloud.alfresco.data.domain.AlfrescoPagedResourcesAssembler;
 import org.activiti.cloud.services.query.app.repository.ProcessInstanceRepository;
 import org.activiti.cloud.services.query.model.ProcessInstanceEntity;
 import org.activiti.cloud.services.query.resources.ProcessInstanceResource;
 import org.activiti.cloud.services.query.rest.assembler.ProcessInstanceResourceAssembler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.hateoas.MediaTypes;
-import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.Resources;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 @ConditionalOnProperty(name = "activiti.enable-clean-up", havingValue = "true")
 @RestController
 @RequestMapping(
-        value = "/admin/v1/process-instances",
+        value = "/admin/clean-up/v1/process-instances",
         produces = {
                 MediaTypes.HAL_JSON_VALUE,
                 MediaType.APPLICATION_JSON_VALUE
@@ -31,31 +32,26 @@ public class ProcessInstanceCleanUpController {
 
     private ProcessInstanceResourceAssembler processInstanceResourceAssembler;
 
-    private AlfrescoPagedResourcesAssembler<ProcessInstanceEntity> pagedResourcesAssembler;
-
     @Autowired
     public ProcessInstanceCleanUpController(ProcessInstanceRepository processInstanceRepository,
-                                          ProcessInstanceResourceAssembler processInstanceResourceAssembler,
-                                          AlfrescoPagedResourcesAssembler<ProcessInstanceEntity> pagedResourcesAssembler) {
+                                          ProcessInstanceResourceAssembler processInstanceResourceAssembler) {
         this.processInstanceRepository = processInstanceRepository;
         this.processInstanceResourceAssembler = processInstanceResourceAssembler;
-        this.pagedResourcesAssembler = pagedResourcesAssembler;
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, path = "/export")
-    public PagedResources<ProcessInstanceResource> exportProcessInstances (@QuerydslPredicate(root = ProcessInstanceEntity.class) Predicate predicate,
-                                                                           Pageable pageable,
-                                                                           boolean delete) {
+    @RequestMapping(method = RequestMethod.DELETE)
+    public Resources<ProcessInstanceResource> deleteProcessInstances (@QuerydslPredicate(root = ProcessInstanceEntity.class) Predicate predicate) {
 
-        PagedResources<ProcessInstanceResource> result = pagedResourcesAssembler.toResource(pageable,
-                processInstanceRepository.findAll(predicate,
-                        pageable),
-                processInstanceResourceAssembler);
+        Collection<ProcessInstanceResource> result = new ArrayList<>();
+        Iterable <ProcessInstanceEntity> iterable = processInstanceRepository.findAll(predicate);
 
-        if(delete)
-        processInstanceRepository.deleteAll();
+        for(ProcessInstanceEntity entity : iterable){
+            result.add(processInstanceResourceAssembler.toResource(entity));
+        }
 
-        return result;
+        processInstanceRepository.deleteAll(iterable);
+
+        return new Resources<>(result);
     }
 
 
