0d2b008f74e46b3ec7206bf4a66eef077ec5fe30
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2019-02-28 10:14:14 +0000

    Refactor taskCleanUpController

--- activiti-cloud-services-query/activiti-cloud-services-query-rest/src/main/java/org/activiti/cloud/services/query/rest/TaskCleanUpController.java
+++ activiti-cloud-services-query/activiti-cloud-services-query-rest/src/main/java/org/activiti/cloud/services/query/rest/TaskCleanUpController.java
@@ -1,26 +1,21 @@
 package org.activiti.cloud.services.query.rest;
 
 import com.querydsl.core.types.Predicate;
-import org.activiti.api.task.model.Task;
-import org.activiti.cloud.alfresco.data.domain.AlfrescoPagedResourcesAssembler;
-import org.activiti.cloud.api.task.model.CloudTask;
-import org.activiti.cloud.services.query.app.repository.EntityFinder;
 import org.activiti.cloud.services.query.app.repository.TaskRepository;
-import org.activiti.cloud.services.query.model.ProcessInstanceEntity;
 import org.activiti.cloud.services.query.model.TaskEntity;
-import org.activiti.cloud.services.query.resources.ProcessInstanceResource;
 import org.activiti.cloud.services.query.resources.TaskResource;
 import org.activiti.cloud.services.query.rest.assembler.TaskResourceAssembler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.hateoas.MediaTypes;
-import org.springframework.hateoas.PagedResources;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 @ConditionalOnProperty(name = "activiti.enable-clean-up", havingValue = "true")
 @RestController
@@ -36,29 +31,24 @@ public class TaskCleanUpController {
 
     private TaskResourceAssembler taskResourceAssembler;
 
-    private AlfrescoPagedResourcesAssembler<TaskEntity> pagedResourcesAssembler;
-
     @Autowired
     public TaskCleanUpController(TaskRepository taskRepository,
-                               TaskResourceAssembler taskResourceAssembler,
-                               AlfrescoPagedResourcesAssembler<TaskEntity> pagedResourcesAssembler) {
+                               TaskResourceAssembler taskResourceAssembler) {
         this.taskRepository = taskRepository;
         this.taskResourceAssembler = taskResourceAssembler;
-        this.pagedResourcesAssembler = pagedResourcesAssembler;
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, path = "/export")
-    public PagedResources<TaskResource> exportTasks (@QuerydslPredicate(root = TaskEntity.class) Predicate predicate,
-                                             Pageable pageable,
-                                             boolean delete) {
+    @RequestMapping(method = RequestMethod.DELETE, path = "/delete")
+    public Collection<TaskResource> deleteTasks (@QuerydslPredicate(root = TaskEntity.class) Predicate predicate) {
 
-        PagedResources<TaskResource> result = pagedResourcesAssembler.toResource(pageable,
-                taskRepository.findAll(predicate,
-                        pageable),
-                taskResourceAssembler);
+        Collection <TaskResource> result = new ArrayList<>();
+        Iterable <TaskEntity> iterable = taskRepository.findAll(predicate);
 
-        if(delete)
-            taskRepository.deleteAll();
+        for(TaskEntity entity : iterable){
+            result.add(taskResourceAssembler.toResource(entity));
+        }
+
+        taskRepository.deleteAll(iterable);
 
         return result;
     }
