e7b2bc1d02daa3a642e4da752af9a19af339b77d
Igor Dianov <igor@dianov.com>
2019-05-28 14:32:41 -0700

    feat: add initial QueryProcessDiagramIT

---
+++ activiti-cloud-starter-query/src/test/java/org/activiti/cloud/starter/tests/QueryProcessDiagramIT.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2018 Alfresco, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.activiti.cloud.starter.tests;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+import org.activiti.api.runtime.model.impl.ProcessDefinitionImpl;
+import org.activiti.api.runtime.model.impl.ProcessInstanceImpl;
+import org.activiti.cloud.api.process.model.impl.events.CloudProcessCreatedEventImpl;
+import org.activiti.cloud.api.process.model.impl.events.CloudProcessDeployedEventImpl;
+import org.activiti.cloud.api.process.model.impl.events.CloudProcessStartedEventImpl;
+import org.activiti.cloud.services.query.app.repository.ProcessDefinitionRepository;
+import org.activiti.cloud.services.query.app.repository.ProcessInstanceRepository;
+import org.activiti.cloud.services.query.app.repository.ProcessModelRepository;
+import org.activiti.cloud.services.query.test.ProcessDefinitionRestTemplate;
+import org.activiti.cloud.services.test.identity.keycloak.interceptor.KeycloakTokenProducer;
+import org.activiti.cloud.starters.test.EventsAggregator;
+import org.activiti.cloud.starters.test.MyProducer;
+import org.apache.commons.io.IOUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:application-test.properties")
+@DirtiesContext
+public class QueryProcessDiagramIT {
+    
+    private static final String PROC_URL = "/v1/process-instances";
+
+    @Autowired
+    private KeycloakTokenProducer keycloakTokenProducer;
+
+    @Autowired
+    private ProcessDefinitionRestTemplate restTemplate;
+
+    @Autowired
+    private ProcessDefinitionRepository processDefinitionRepository;
+
+    @Autowired
+    private ProcessModelRepository processModelRepository;
+
+    @Autowired
+    private ProcessInstanceRepository processInstanceRepository;
+    
+    @Autowired
+    private MyProducer producer;
+    
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+    
+    private String processDefintionId = UUID.randomUUID().toString();
+    
+    private EventsAggregator eventsAggregator;
+
+    @Before
+    public void setUp() throws IOException {
+        keycloakTokenProducer.setKeycloakTestUser("hruser");
+        
+        eventsAggregator = new EventsAggregator(producer);
+        
+        //given
+        ProcessDefinitionImpl firstProcessDefinition = new ProcessDefinitionImpl();
+        firstProcessDefinition.setId(processDefintionId);
+        firstProcessDefinition.setKey("mySimpleProcess");
+        firstProcessDefinition.setName("My Simple Process");
+
+        CloudProcessDeployedEventImpl firstProcessDeployedEvent = new CloudProcessDeployedEventImpl(firstProcessDefinition);
+        firstProcessDeployedEvent.setProcessModelContent(IOUtils.toString(
+                Thread.currentThread().getContextClassLoader().getResourceAsStream("parse-for-test/SimpleProcess.bpmn20.xml"),
+                                                                                   StandardCharsets.UTF_8));
+        producer.send(firstProcessDeployedEvent);
+    }
+
+    @After
+    public void tearDown() {
+        processModelRepository.deleteAll();
+        processDefinitionRepository.deleteAll();
+        processInstanceRepository.deleteAll();
+    }
+
+    @Test
+    public void shouldGetProcessInstanceDiagram() {
+        //given
+        ProcessInstanceImpl process = new ProcessInstanceImpl();
+        process.setId("process-instance-id");
+        process.setName("process");
+        process.setProcessDefinitionKey("mySimpleProcess");
+        process.setProcessDefinitionId(processDefintionId);
+        process.setProcessDefinitionVersion(1);
+        
+        eventsAggregator.addEvents(new CloudProcessCreatedEventImpl(process),
+                                   new CloudProcessStartedEventImpl(process,
+                                                       null,
+                                                       null));
+        eventsAggregator.sendAll();
+        
+        await().untilAsserted(() -> {
+
+            //when
+            ResponseEntity<String> responseEntity = testRestTemplate.exchange(PROC_URL + "/" + process.getId() + "/diagram",
+                                                                                       HttpMethod.GET,
+                                                                                       keycloakTokenProducer.entityWithAuthorizationHeader(),
+                                                                                       String.class);
+            //then
+            assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+            assertThat(responseEntity.getBody()).isNotNull();
+        });
+    }    
+}
